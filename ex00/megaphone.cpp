24953e55b8ecfbcbfbda613e3bc7070d4e2e7b03
anisdjl <adjelili954@gmail.com>
2026-04-25 22:58:57 +0200

    cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,20 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anis <anis@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/21 17:05:25 by adjelili          #+#    #+#             */
-/*   Updated: 2026/04/21 17:39:43 by adjelili         ###   ########.fr       */
+/*   Created: 2026/04/25 22:07:19 by anis              #+#    #+#             */
+/*   Updated: 2026/04/25 22:58:24 by anis             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string.h>
 
-int	main()
+int	main(int argc, char **argv)
 {
-	int	var;
+	int	a;
+	int	y;
 
-	var = 10;
-	std::cout << var << std::endl;
+	a = 1;
+	(void)argc;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (argv[a])
+	{
+		y = 0;
+		while (argv[a][y])
+		{
+			if (std::isalpha(argv[a][y]))
+				std::cout << (char)std::toupper(argv[a][y]);
+			else
+				std::cout << argv[a][y];
+			y++;
+		}
+		a++;
+	}
+	std::cout << std::endl;
 	return (0);
 }
