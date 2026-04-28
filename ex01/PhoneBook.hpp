462db8c86135f91191d1f96fb95ca696ed2d79e0
anisdjl <adjelili954@gmail.com>
2026-04-28 18:19:03 +0200

    cpp

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -3,19 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anis <anis@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 23:15:27 by anis              #+#    #+#             */
-/*   Updated: 2026/04/25 23:17:07 by anis             ###   ########.fr       */
+/*   Updated: 2026/04/28 15:34:58 by adjelili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_H
 # define PHONEBOOK_H
 
+#include <iostream>
+#include <iomanip>
+
 class PhoneBook
 {
-	public:	
+	public:
+	std::string s;
 };
 
 #endif
--- ex01/phone_book.cpp
+++ ex01/phone_book.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   phone_book.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anis <anis@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 23:15:24 by anis              #+#    #+#             */
-/*   Updated: 2026/04/25 23:20:42 by anis             ###   ########.fr       */
+/*   Updated: 2026/04/28 15:52:32 by adjelili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 int	main(void)
 {
+	PhoneBook	phonebook;
+	std::string		input;
+
+	while (1)
+	{
+		std::cout << "Enter an option : ADD | SEARCH | EXIT\n";
+		std::cin >> input;
+		std::cout << input << std::endl;
+		if (input.compare("ADD"))
+		{
+			
+		}
+	}
 	return (0);	
 }
